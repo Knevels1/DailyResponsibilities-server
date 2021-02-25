78ba16009b9b97ec11c06f86f1317c945d0908e5
Knevels1 <kyle.a.nevels@icloud.com>
2021-02-25 15:55:53 -0600

    seeded data

--- DailyTaskApi/models/tasks.py
+++ DailyTaskApi/models/tasks.py
@@ -5,6 +5,6 @@ class Task(models.Model):
 
     user = models.ForeignKey("TaskUser", on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
-    publication_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    task_date = models.DateField(auto_now=False, auto_now_add=False)
     content = models.CharField(max_length=2500)
     complete = models.BooleanField()
--- seed.sh
+++ seed.sh
@@ -2,9 +2,9 @@
 rm -rf DailyTaskapi/migrations
 rm db.sqlite3
 python manage.py migrate
-python manage.py makemigrations DailyTaskapi
-python manage.py migrate DailyTaskapi
+python manage.py makemigrations DailyTaskApi
+python manage.py migrate DailyTaskApi
 python manage.py loaddata users
 python manage.py loaddata tokens
-python manage.py loaddata my_user
+python manage.py loaddata task_users
 python manage.py loaddata task
